5ba8e016e88367b2947c6ad1a1d59ccaba2cb19c
Aryaman Mishra <aryaman.mishra73@gmail.com>
2024-06-28 16:59:39 +0530

    Added Clerk Authentication

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarLinks =
+[
+    {
+        label:'Home',
+        route:'/',
+        imgUrl:'/icons/Home.svg'
+    },
+    {
+        label:'Upcoming Exams',
+        route:'/upcoming',
+        imgUrl:'/icons/upcoming.svg'
+    },
+    {
+        label:'Previous Exams',
+        route:'/previous',
+        imgUrl:'/icons/previous.svg'
+    },
+    {
+        label:'Exam Recordings',
+        route:'/recordings',
+        imgUrl:'/icons/Video.svg'
+    },
+    {
+        label:'Personal Exam Room',
+        route:'/personal-room',
+        imgUrl:'/icons/add-personal.svg'
+    },
+]
