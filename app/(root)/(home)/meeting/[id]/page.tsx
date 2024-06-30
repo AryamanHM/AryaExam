6d27c287817d3dbe456d6f2fac01156007f8f6e4
Aryaman Mishra <aryaman.mishra73@gmail.com>
2024-07-01 01:40:28 +0530

    Camera property of call not found
    Try to rectify it

--- app/(root)/(home)/meeting/[id]/page.tsx
+++ app/(root)/(home)/meeting/[id]/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+"use client"
+import MeetingRoom from '@/components/MeetingRoom'
+import MeetingSetup from '@/components/MeetingSetup'
+import { useGetCallByID } from '@/hooks/useGetCallByID'
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
+import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params:{ id } }: { params: { id: string } }) => {
+  const { user,isLoaded }=useUser() 
+  const [isSetupComplete, setisSetupComplete] = useState(false)
+  const { call,isCallLoading }=useGetCallByID(id);
+  if(!isLoaded || isCallLoading)
   return (
-    <div>Meeting Room: #{params.id}</div>
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup />
+          ):(
+            <MeetingRoom />
+          )}
+
+        </StreamTheme>
+      </StreamCall>
+    </main>
+    //<div>Meeting Room: #{params.id}</div>
   )
 }
 
